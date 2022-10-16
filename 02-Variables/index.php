3e72d9bc29aa8d6d8b837d4cde3b34eee99be693
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 16:48:22 +0200

    Variables constantes

---
+++ 02-Variables/index.php
@@ -0,0 +1,12 @@
+<?php
+//variable constantes
+define('HOST','localhost');
+define('USER','root');
+//echo USER;
+ var_dump(USER);
+// print_r(HOST);
+$x=4;
+$y=2;
+$z=5;
+$calcule=$x+$y;
+echo $calcule;
