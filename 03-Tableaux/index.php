3e30d7068766958768f93f3457d770bf5e3a7ca4
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 17:02:50 +0200

    Remplire un tableau

--- 03-Tableaux/index.php
+++ 03-Tableaux/index.php
@@ -3,3 +3,8 @@
 $tableau = array();
 //deux methode pour declarer
 $tableau = [];
+//remplir un tableau
+$tableau2 = array('lundi', 'mardi', 10, 55, true);
+
+$tableau = [false, 'test', 5.20, [66, 20, 13]];
+echo '<pre>';
