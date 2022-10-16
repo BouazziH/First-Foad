e26b35d994743128f5534c39f067d38a4c51777b
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 17:44:24 +0200

    First exercices

---
+++ Travaux/travail-00.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ Travail-00 :
+    Créer un tableau php avec ses nombres : 27,15,34,379,248,5643,81,211,999,142,300,572
+    Tester les nombres de ce tableau pour savoir si chaque nombre est pair ou impair (un nombre est pair si le resultat de la division par 2 est 0)
+    Afficher les résultats sous forme de liste html ul comme ceci (xx,yy sont des nombres) :
+* xx : pair
+* yy : impaire
+etc ...
+ */
+ 
+$tab0=[27,15,34,379,248,5643,81,211,999,142,300,572];
+
+for ($i=0; $i < count($tab0) ; $i++) 
+{ echo '<ul>';
+   // print_r($tab0[$i].',');
+    if ($tab0[$i]%2==0 )
+     {
+        echo "<li>$tab0[$i] : est paire </li><br>";
+
+    }
+    else{
+        echo "<li>$tab0[$i] : est impaire </li><br>";
+    }
+    echo '</ul>';
+}
