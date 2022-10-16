1559b53d7c649539ed5d85a8d5431c8d08dad884
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 16:38:48 +0200

    How to declare variables with php

--- 01-Syntaxe/index.php
+++ 01-Syntaxe/index.php
@@ -1,6 +1,37 @@
 <h1> Syntaxe</h1>
 <?php
 //echo/print
-echo'test en php',' autre teste <br>';
+echo 'test en php', ' autre teste <br>';
 //print c'est le moin utiliser en php
-print' texte en php avec print <br>';
+print ' texte en php avec print <br>';
+// declaration variable
+$nom = 'Bouazzi';
+$age = 28;
+echo $nom, ' ', $age;
+//differente maniere de declaration de variable
+$camelCase = 'une variable en camel case';
+$snake_case = 'une variable en snake case';
+$pascaleCase = 'une variable pascale case';
+$var = 'une variable en kebab-case';
+//string
+$nom = 'abc';
+//nombre
+$int = 10;
+//float 
+$note = 15.36;
+//bool
+$var = true;
+$var = false;
+//array/objet
+$tableau = ['groupe', 21, 5, 23, false];
+//null
+$var = null;
+
+// simple/double guillemets
+$message = "<br>Aujroud'hui";
+$mess = 'aujourd\'hui';
+echo $mess;
+
+$txt = 'bonjou';
+echo '$txt tout le monde';
+echo "$txt tout le mondss";
