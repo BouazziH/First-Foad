b5bff46b00f574d680a022063416eae1fbb4888d
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 16:54:02 +0200

    Affecter des var

--- 02-Variables/index.php
+++ 02-Variables/index.php
@@ -10,3 +10,11 @@ $y=2;
 $z=5;
 $calcule=$x+$y;
 echo $calcule;
+// affectation
+$a=5;
+$b=7;
+//$a +=6;
+$a /= $b;
+//echo '<br>'.$a;
+//round c'est une fonction qui permet de specifier le nombre apres la vergule
+echo '<br>'.round($a,3).'<br>';
