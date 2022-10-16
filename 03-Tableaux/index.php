04a244a4ff4b74da283abecf1756a80e5a9a33b6
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 17:10:30 +0200

    Fichier html pour afficher un tab

--- 03-Tableaux/index.php
+++ 03-Tableaux/index.php
@@ -192,3 +192,27 @@ print_r($produits);
 
 
 echo '</pre>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Produits</h1>
+    <ul>
+        <?php foreach ($produits as $produit) { ?>
+            <li>
+                <h3><?php echo $produit['nom']; ?></h3>
+                <p><?php echo $produit['prix']; ?> €</p>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
