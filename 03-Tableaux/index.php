139c137b0215f81395d199a81769ee568414a193
BouazziH <104574763+BouazziH@users.noreply.github.com>
2022-10-16 17:03:39 +0200

    Ajouter des var au array

--- 03-Tableaux/index.php
+++ 03-Tableaux/index.php
@@ -8,3 +8,13 @@ $tableau2 = array('lundi', 'mardi', 10, 55, true);
 
 $tableau = [false, 'test', 5.20, [66, 20, 13]];
 echo '<pre>';
+//ajouter une variable au tableau
+array_push($tableau, 'voiture'); //une manierre pour ajouter une valeur sur le tableau
+array_push($tableau, 'voiture1', 'valeur2'); //on peut ajouter meme plusieur valeurs 
+$tableau[] = 'hamza';
+$tableau[] = 'titre se sejour';
+$tableau[] = 'stage';
+$tableau[] = "billet d'avion";
+//echo count($tableau); //pour s'avoir combien d'element sur mon tableau
+//echo var_dump($tableau); // afichage du tableau
+echo '<hr>';
